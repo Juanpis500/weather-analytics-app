0e47bde6317af1d527c778f69942e363684b06c7
Juanpis500 <juanpis65@hotmail.com>
2026-09-14 09:59:47 -0600

    fix: add new url from backend service

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://weather-backend-a1ri.onrender.com"
     ]
 
     class Config:
