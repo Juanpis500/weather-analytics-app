138176b56fa460a26f89ce20baf9c60f2422000d
Juanpis500 <juanpis65@hotmail.com>
2026-09-14 10:14:12 -0600

    fix: add new url for CORS security

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,7 +13,8 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://weather-backend-a1ri.onrender.com"
+        "https://weather-backend-a1ri.onrender.com",
+        "https://weather-analytics-app-green.vercel.app"
     ]
 
     class Config:
